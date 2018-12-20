b6682249c5992b4b881f56d2813300239fc1763d
jose MARIN <josermarinr18@gmail.com>
2018-12-19 23:40:19 -0400

    elements to css and sass

---
+++ revisar -sass+js-/1/script.js
@@ -0,0 +1,11 @@
+
+  $(document).ready( function animate() {
+    document.querySelector('blockquote').classList.toggle('animate');
+  
+  
+  document.addEventListener('click', animate);
+  
+  setTimeout(animate, 1000);
+}
+    
+    
